8926394c1ba9759e1fb0c1db7821bed2efd4a00d
AlexK85 <saharr.k@mail.ru>
2020-10-06 22:30:28 +0300

    add task_1
    Добавил Задачу 1

---
+++ script.php
@@ -0,0 +1,27 @@
+<?php 
+   echo "Введите фамилию: ";
+   $lastname = trim(fgets(STDIN));
+
+   echo "Введите имя: ";
+   $name = trim(fgets(STDIN));
+
+  do {
+    echo "Введите возраст: ";
+    $age = (int) trim(fgets(STDIN));
+  } 
+  while($age <= 0);
+   
+  //  $lastname . $name . $age . "\n";
+  //  var_dump($lastname);
+  //  var_dump($name);
+  //  var_dump($age);
+   
+  // if ($age === 0) {
+  //   echo '!';
+  //   echo "Введите возраст: ";
+  //   $age = (int) trim(fgets(STDIN));
+  // } 
+  // if ($age > 0) {
+  //   echo 'Спасибо! Вы ввели все данные!';
+  // }
+  
